5d240319c52f7ce0cf64cc5470431ed3860a9401
Luis Gonzalez <lgonzalez.us1@gmail.com>
2020-05-14 15:28:52 -0400

    third  commit

---
+++ Bookstore.java
@@ -0,0 +1,75 @@
+import java.util.Date;
+import java.text.*;
+
+public class Bookstore{
+
+    public String Name;
+    public String Address;
+    public int SqFt;
+    public boolean HasNewBooks;
+    public boolean HasUsedBooks;
+
+    private boolean Open;
+    private Date currentTime = new Date();
+    
+    private static final int OPENING_TIME = 7 ;
+    private static final int CLOSING_TIME = 20 ;
+
+    //Public Methods
+
+    public Bookstore() {
+        this.Name = "Your Bookstore Name";
+        this.Address = "1234 Books Ave";
+        this.SqFt = 1300;
+        this.HasNewBooks = true;
+        this.HasUsedBooks = true;
+        this.Open = this.isOpen(currentTime); //needs to be adjusted;
+    }
+
+    public Bookstore(String name, String address) {
+        this.Name = name;
+        this.Address = address;
+        this.SqFt = 1000;
+        this.HasNewBooks = true;
+        this.HasUsedBooks = true;
+        this.Open = this.isOpen(currentTime); //needs to be adjusted;
+    }
+
+    public Bookstore(String name, String address, int sqft) {
+        this.Name = name;
+        this.Address = address;
+        this.SqFt = sqft;
+        this.HasNewBooks = true;
+        this.HasUsedBooks = true;
+        this.Open = this.isOpen(currentTime); //needs to be adjusted;
+    }
+
+    public void getStoreHours() {
+        System.out.println("Store Hours are from " + OPENING_TIME + ":00 to " + CLOSING_TIME + ":00.");
+    }
+
+    public boolean isOpen() {
+        if (Open) {
+            System.out.println("Yes, the store is open right now");
+            return true;
+        } else {
+            System.out.println("No, the store is currently closed");
+            return false;
+        }
+    }
+
+    // Private Internal Methods
+
+    private boolean isOpen(Date date) {
+        SimpleDateFormat ft = new SimpleDateFormat("H");
+
+        int time = Integer.parseInt(ft.format(date));
+        
+        if (time > OPENING_TIME && time < CLOSING_TIME) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}    
